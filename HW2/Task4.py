dfdc9eae07058e6fdd63645bd413abf8a896dd88
Bohdan <bohdan99@Ubuntu.myguest.virtualbox.org>
2023-05-09 22:59:54 +0300

    Task 4 can be reviewed

---
+++ HW2/Task4.py
@@ -0,0 +1,11 @@
+'''
+You have a group of guests for the VIP box. The seats in the VIP box are all occupied by these guests and can not be
+changed. There is a group of guests in the common room and there are still places in it.
+Display 2 groups of guests in code.
+'''
+
+vip_room = frozenset({'Sam', 'Din', 'Marry', 'Ken'})
+common_room = {'Joe', 'Helen', 'Mike'}
+
+print(vip_room)
+print(common_room)
