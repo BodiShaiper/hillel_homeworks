d37b794c8027f3e3922d418778ea802bd7926bd5
Bohdan <bohdan99@Ubuntu.myguest.virtualbox.org>
2023-05-11 23:30:15 +0300

    HW3 tasks completed

---
+++ HW3/Task2.py
@@ -0,0 +1,18 @@
+'''
+You have the number 2 as a variable. Multiply it by 2 in two ways.
+Accordingly, you need to divide it in 2 different ways by 2.
+'''
+
+x = 2
+
+multiple_1 = x * 2
+multiple_2 = x + x
+multiple_3 = x << 1
+
+print(multiple_1, multiple_2, multiple_3)
+
+divide_1 = x / 2
+divide_2 = x // 2
+divide_3 = x >> 1
+
+print(divide_1, divide_2, divide_3)
