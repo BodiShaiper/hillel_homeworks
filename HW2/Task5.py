e577c8a4be08c8c51f7fba7d2ec05dd807458187
Bohdan <bohdan99@Ubuntu.myguest.virtualbox.org>
2023-05-11 22:21:48 +0300

    no if/loops -> pain

--- HW2/Task5.py
+++ HW2/Task5.py
@@ -6,10 +6,19 @@ task. If there are 2 johns in the list, you need to take only one of them.
 
 group_of_people = ['John', 'Erica', 'Sam', 'John', 'John', 'John', 'Ben', 'Ben']
 
-unique_names_list = []
-
+'''
 for name in group_of_people:
     if group_of_people.count(name) == 1:
         unique_names_list.append(name)
 
+print(unique_names_list)
+'''
+
+unique_names_list = []
+group_of_people.sort()
+print(group_of_people)
+unique_names_list.append(group_of_people[0])
+unique_names_list.append(group_of_people[2])
+unique_names_list.append(group_of_people[3])
+unique_names_list.append(group_of_people[-1])
 print(unique_names_list)
