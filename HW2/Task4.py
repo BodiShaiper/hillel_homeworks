949fd539b096844abc7094e7d5bb76a3b70afdaf
Bohdan <bohdan99@Ubuntu.myguest.virtualbox.org>
2023-05-11 22:19:59 +0300

    Feedback was taken into account

--- HW2/Task4.py
+++ HW2/Task4.py
@@ -4,8 +4,8 @@ changed. There is a group of guests in the common room and there are still place
 Display 2 groups of guests in code.
 '''
 
-vip_room = frozenset({'Sam', 'Din', 'Marry', 'Ken'})
-common_room = {'Joe', 'Helen', 'Mike'}
+vip_room = frozenset({'Place_1': 'Sam', 'Place_2': 'Din', 'Place_4': 'Marry', 'Place_5': 'Ken'})
+common_room = {'Place_1': None, 'Place_2': None, 'Place_3': 'Mike', 'Place_4': None}
 
 print(vip_room)
 print(common_room)
