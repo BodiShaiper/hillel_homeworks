8b3629f26a20dc310f13af460d3da53ee874998a
Bohdan <bohdan99@Ubuntu.myguest.virtualbox.org>
2023-05-16 20:38:19 +0300

    Fixed issue with task5

--- HW2/Task5.py
+++ HW2/Task5.py
@@ -4,7 +4,8 @@ task. If there are 2 johns in the list, you need to take only one of them.
 "John Dow", "John Dow", "Marta Dow" => "John Dow", "Marta Dow ")
 '''
 
-group_of_people = ['John', 'Erica', 'Sam', 'John', 'John', 'John', 'Ben', 'Ben']
+group_of_people = frozenset(['John', 'Erica', 'Sam', 'John', 'John', 'John', 'Ben', 'Ben'])
+print(group_of_people)
 
 '''
 for name in group_of_people:
@@ -13,12 +14,3 @@ for name in group_of_people:
 
 print(unique_names_list)
 '''
-
-unique_names_list = []
-group_of_people.sort()
-print(group_of_people)
-unique_names_list.append(group_of_people[0])
-unique_names_list.append(group_of_people[2])
-unique_names_list.append(group_of_people[3])
-unique_names_list.append(group_of_people[-1])
-print(unique_names_list)
